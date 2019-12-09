ea8885dcdf4015b366847d8ce27795a7a1c10a02
Stefano Gerli <sgerli@igerli.com>
2019-12-09 00:11:55 -0600

    Finish part 1 and 2

--- day9.py
+++ day9.py
@@ -1,10 +1,11 @@
 import sys
 import itertools
 
-VERBOSE = False
+VERBOSE = True
 
 def openFile():
     return open(sys.argv[1], "r")
+
 class IntcodeMachine:
     def __init__(self, data):
         self.memory = data
@@ -24,85 +25,83 @@ class IntcodeMachine:
             paramModes = str(self.memory[self.pointer])[:-2]
             if (opcode == 99): # HALT
                 self.halt = True
+                if VERBOSE:
+                    print('END')
                 break
             elif (opcode == 1): # OPCODE 01 - Sum
-                self.memory[self.memory[self.pointer + 3]] = self.getParam(paramModes,1) + self.getParam(paramModes,2)
+                self.__setParam(paramModes, 3, self.__getParam(paramModes,1) + self.__getParam(paramModes,2))
                 self.pointer += 4
             elif (opcode == 2): # OPCODE 02 - Multiply
-                self.memory[self.memory[self.pointer + 3]] = self.getParam(paramModes,1) * self.getParam(paramModes,2)
+                self.__setParam(paramModes, 3, self.__getParam(paramModes,1) * self.__getParam(paramModes,2))
                 self.pointer += 4
             elif (opcode == 3):  # OPCODE 03 - Input
                 if inputArg != None:
-                    self.memory[self.memory[self.pointer + 1]] = inputArg
+                    self.__setParam(paramModes, 1, inputArg)
                     inputArg = None
                 else:
                     if VERBOSE:
-                        self.memory[self.memory[self.pointer + 1]] = int(input("Type int > "))
+                        self.__setParam(paramModes, 1, int(input("Type int > ")))
                     else:
                         break
                 self.pointer += 2
             elif (opcode == 4):  # OPCODE 04 - Print
-                out = self.getParam(paramModes,1)
+                out = self.__getParam(paramModes,1)
                 if VERBOSE:
                     print(">> " + str(out))
                 self.out = out
                 self.pointer += 2
             # Start of part 2
             elif (opcode == 5):  # OPCODE 05 - JNZ (jump if not zero)
-                if (self.getParam(paramModes,1) != 0):
-                    self.pointer = self.getParam(paramModes,2)
+                if (self.__getParam(paramModes,1) != 0):
+                    self.pointer = self.__getParam(paramModes,2)
                 else:
                     self.pointer += 3
             elif (opcode == 6):  # OPCODE 06 - JZ (jump if zero)
-                if (self.getParam(paramModes,1) == 0):
-                    self.pointer = self.getParam(paramModes,2)
+                if (self.__getParam(paramModes,1) == 0):
+                    self.pointer = self.__getParam(paramModes,2)
                 else:
                     self.pointer += 3
             elif (opcode == 7):  # OPCODE 07 - Set 1 if first is less than second else 0
-                if (self.getParam(paramModes,1) < self.getParam(paramModes,2)):
-                    self.memory[self.memory[self.pointer + 3]] = 1
+                if (self.__getParam(paramModes,1) < self.__getParam(paramModes,2)):
+                    self.__setParam(paramModes, 3, 1)
                 else:
-                    self.memory[self.memory[self.pointer + 3]] = 0
+                    self.__setParam(paramModes, 3, 0)
                 self.pointer += 4
             elif (opcode == 8):  # OPCODE 08 - Set 1 if first is equal to second else 0
-                if (self.getParam(paramModes,1) == self.getParam(paramModes,2)):
-                    self.memory[self.memory[self.pointer + 3]] = 1
+                if (self.__getParam(paramModes,1) == self.__getParam(paramModes,2)):
+                    self.__setParam(paramModes, 3, 1)
                 else:
-                    self.memory[self.memory[self.pointer + 3]] = 0
+                    self.__setParam(paramModes, 3, 0)
                 self.pointer += 4
+            elif opcode == 9:
+                self.relBase += self.__getParam(paramModes,1)
+                self.pointer += 2
             else:
                 print(str(self.memory[self.pointer]) + " Something went wrong :(")
                 break
 
-    def getParam(self, paramModes, param):
+    def __getParam(self, paramModes, param):
+        return self.memory[self.__getParamAddress(paramModes, param)]
+
+    def __setParam(self, paramModes, param, value):
+        self.memory[self.__getParamAddress(paramModes, param)] = value
+    
+    def __getParamAddress(self, paramModes, param):
         type = 0
         try:
             type = int(paramModes[-param])
         except:
             pass
         if type == 0:
-            return self.memory[self.memory[self.pointer + param]]
-        elif type == 1:
             return self.memory[self.pointer + param]
-        elif type == 2:
-        	return self.memory[self.relBase + param]
-        else:
-        	print('Error')
-        	
-    def setParam(self, paramModes, param, value):
-        type = 0
-        try:
-            type = int(paramModes[-param])
-        except:
-            pass
-        if type == 0:
-            self.memory[self.memory[self.pointer + param]] = vql
         elif type == 1:
-            return self.memory[self.pointer + param]
+            return self.pointer + param
         elif type == 2:
-        	return self.memory[self.relBase + param]
+            offset = self.memory[self.pointer + param]
+            return self.relBase + offset
         else:
-        	print('Error')
+            print('Param type error')
+            return 0
 
 def challenges():
     """
